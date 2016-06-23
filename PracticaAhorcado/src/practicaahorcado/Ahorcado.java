f815e671f796e0ee062e94b218d15ddd2eec9c0d
jorgerugalde <jrojasu@ucatolica.ac.cr>
2016-06-22 23:13:02 -0600

    Avance Proyecto

--- PracticaAhorcado/src/practicaahorcado/Ahorcado.java
+++ PracticaAhorcado/src/practicaahorcado/Ahorcado.java
@@ -14,54 +14,45 @@ public class Ahorcado {
      */
     public static void main(String[] args){
         // TODO code application logic here
-        String [] palabras=
-        //Palabras
-        {"Saprissa".toUpperCase(),
-        "Universidad".toUpperCase(),
-        "Zapato".toUpperCase(),
-        "Carro".toUpperCase(),
-        "Alajuelense".toUpperCase(),
-        "Heredia".toUpperCase(),
-        "Ingenieria".toUpperCase(),
-        "Sistemas".toUpperCase(),
-        "Perro".toUpperCase(),
-        "Gato".toUpperCase(),
-        "Caballo".toUpperCase(),
-        "Naranja".toUpperCase(),
-        "Manzana".toUpperCase(),
-        "Profesor".toUpperCase(),
-        "Telefono".toUpperCase(),
-        "Argentina".toUpperCase(),
-        "Alemania".toUpperCase(),
-        "Brasil".toUpperCase(),
-        "Uruguay".toUpperCase(),};
-        String linea, palabra=palabras[(int)(Math.random()*palabras.length)];
-        int i, n=palabra.length(), turnos =0, aciertos = 0, oportunidades=6;
+String palabras []=new String [10];
+        String palabras2[]=new String [10];
+        String palabraEscogida;
+        String palabraOculta[];
+        String letraElegida;
+        //palabras
+        palabras[0]="ALAJUELA".toUpperCase();
+        palabras[1]="HEREDIA".toUpperCase();
+        palabras[2]="limon".toUpperCase();
+        palabras[3]="cartago".toUpperCase();
+        palabras[4]="puntarenas".toUpperCase();
+        palabras[5]="guanacaste".toUpperCase();
+        palabras[6]="miami".toUpperCase();
+        palabras[7]="europa".toUpperCase();
+        palabras[8]="brasil".toUpperCase();
+        palabras[9]="españa".toUpperCase();
+        //
+        String linea, palabra=palabras[(int)(Math.random()* palabras.length)];
+        int i, n=palabra.length(), turnos=0, aciertos=0, oportunidades=6;
         char letra, caracter;
-        char[] casillas=new char[n];
+        char casillas[]=new char[n];
         boolean encontrado;
-        for (i=0;i<n; i++)
-        {
+        for (i=0; i<n; i++) {
             casillas[i]='_';
         }
-        Scanner teclado;
-        PrintStream out;
-        do
-        {
-            System.out.println("Oportunidades restantes: " + (oportunidades=turnos));
+        do {
+            System.out.println("Oportunidades restantes: "+(oportunidades=turnos));
             for (i=0;i<n;i++)
             {
-                System.out.println(""+casillas[1]);
+                System.out.println(""+casillas[i]);
             }
-            System.out.println("\nEscriba una letra; ");
-            do
-            {
-                linea=(JOptionPane.showInputDialog(null, "Escriba una letra", "Letra", JOptionPane.QUESTION_MESSAGE));
+            System.out.println("\nEscriba una letra: ");
+            do{
+                linea=(JOptionPane.showInputDialog(null, "Escriba una letra", "letra", JOptionPane.QUESTION_MESSAGE));
             }
             while(linea.isEmpty());
             letra=linea.charAt(0);
-            encontrado= false;
-            for(i=0;i<n; i++)
+            encontrado=false;
+            for(i=0;i<n;i++)
             {
                 caracter=palabra.charAt(i);
                 if(Character.toUpperCase(letra)==Character.toUpperCase(caracter))
@@ -74,21 +65,18 @@ public class Ahorcado {
                     }
                 }
             }
-            if(!encontrado)
-            {
+            if(!encontrado) {
                 turnos++;
                 System.out.println("Letra no encontrada");
             }
         }
         while(turnos<oportunidades && aciertos<n);
-        if (aciertos==n)
-        {
+        if (aciertos==n){
             System.out.println("Felicidades, has ganado");
         }
-        else
-        {
-            System.out.println("Has Perdido");
+        else{
+            System.out.println("Has perdido y quedas colgado");
         }
-        System.out.println("La palabra secreta era: "+palabra+ "\n\n");
-        }
+        System.out.println("La palabra secreta era: " + palabra + "\n");
     }
+}
